e99951c471b87b1a5b0fea228fe75ca927ab072d
Gabriel Serejo <gabriel_serejo@live.com>
2024-04-01 22:48:30 -0300

    :bug: Bugfix dayjs invalid path

--- utils/cms/CmsController.ts
+++ utils/cms/CmsController.ts
@@ -1,8 +1,8 @@
 import type { MarkdownRoot } from '@nuxt/content/types'
 import dayjs from 'dayjs'
 import { visit } from 'unist-util-visit'
-import duration from 'dayjs/plugin/duration'
-import relativeTime from 'dayjs/plugin/relativeTime'
+import duration from 'dayjs/plugin/duration.js'
+import relativeTime from 'dayjs/plugin/relativeTime.js'
 
 import type { CmsEntry } from './types'
 import { CmsEntrySchema } from './schemas'
