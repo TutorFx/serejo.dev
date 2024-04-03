91143320a6b499a3cd2651616e6210a02b130f6c
Gabriel Serejo <gabriel_serejo@live.com>
2024-04-03 14:14:53 -0300

    :mag_right: Move the Individual pages to the ContentRenderer component

--- components/History/Selector.vue
+++ components/History/Selector.vue
@@ -133,7 +133,7 @@ const current = ref()
                   <div class="font-bold text-4xl">
                     {{ $t('description') }}
                   </div>
-                  <MDC class="text-lg prose prose-md md:text-xl text-pretty max-w-none" :value="current" />
+                  <ContentRenderer class="text-lg prose prose-md md:text-xl text-pretty max-w-none" :value="current" />
                 </div>
               </div>
             </div>
