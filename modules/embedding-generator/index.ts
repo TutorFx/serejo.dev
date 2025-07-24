c3cba0b5173e04bb64a0217e105d2d72dfce87f0
Gabriel Serejo <gabriel_serejo@live.com>
2025-07-24 16:19:34 -0300

    feat: condition to embed only on dev

--- modules/embedding-generator/index.ts
+++ modules/embedding-generator/index.ts
@@ -20,48 +20,51 @@ export default defineNuxtModule({
       date: true
     }})
 
-    const indexWithEmbeddings = new Index({
-      url: process.env.UPSTASH_VECTOR_REST_URL,
-      token: process.env.UPSTASH_VECTOR_REST_TOKEN,
-    });
+    if (process.env.UPSTASH_VECTOR_REST_URL && process.env.UPSTASH_VECTOR_REST_TOKEN) {
+      const indexWithEmbeddings = new Index({
+        url: process.env.UPSTASH_VECTOR_REST_URL,
+        token: process.env.UPSTASH_VECTOR_REST_TOKEN,
+      });
+  
+      const vectorStore = new UpstashVectorStore(new FakeEmbeddings(), {
+        index: indexWithEmbeddings,
+      });
+  
+      const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 500,
+        chunkOverlap: 50,
+      });
+  
+      let documents: Document[] = []
+  
+      nuxt.hooks.hook('content:file:afterParse', async (ctx) => {
+        const { file } = ctx;
+  
+        const { data: headerData } = parseFrontMatter(frontMatterWithDividersRegex.exec(file.body)?.at(0) ?? '')
+        const body = file.body.replace(frontMatterWithDividersRegex, '')
+        
+        if (!body) return;
+  
+        const texts = await textSplitter.splitText(body);
+  
+  
+        documents = documents.concat(texts.map((text, index) => {
+          return {
+            pageContent: text,
+            metadata: { 
+              ...headerData,
+              source: file.id,
+            },
+            id: file.id + ':' + index
+          } satisfies Document
+        }))
+      })
+  
+      nuxt.hooks.hook('ready', async (ctx) => {
+        logger.log('🦜 Documentos a serem analizados:', documents.length)
+        await vectorStore.addDocuments(documents);
+      })
+    }
 
-    const vectorStore = new UpstashVectorStore(new FakeEmbeddings(), {
-      index: indexWithEmbeddings,
-    });
-
-    const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 500,
-      chunkOverlap: 50,
-    });
-
-    let documents: Document[] = []
-
-    nuxt.hooks.hook('content:file:afterParse', async (ctx) => {
-      const { file } = ctx;
-
-      const { data: headerData } = parseFrontMatter(frontMatterWithDividersRegex.exec(file.body)?.at(0) ?? '')
-      const body = file.body.replace(frontMatterWithDividersRegex, '')
-      
-      if (!body) return;
-
-      const texts = await textSplitter.splitText(body);
-
-
-      documents = documents.concat(texts.map((text, index) => {
-        return {
-          pageContent: text,
-          metadata: { 
-            ...headerData,
-            source: file.id,
-          },
-          id: file.id + ':' + index
-        } satisfies Document
-      }))
-    })
-
-    nuxt.hooks.hook('ready', async (ctx) => {
-      logger.log('🦜 Documentos a serem analizados:', documents.length)
-      await vectorStore.addDocuments(documents);
-    })
   }
 })
