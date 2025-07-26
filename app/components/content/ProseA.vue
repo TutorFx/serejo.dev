032eb890af72682aa5f56e5b16b139493849e5ce
Gabriel Serejo <gabriel_serejo@live.com>
2025-07-26 11:06:36 -0300

    style: lint fixed prose a

--- app/components/content/ProseA.vue
+++ app/components/content/ProseA.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+const props = defineProps({
+  href: {
+    type: String,
+    default: '',
+  },
+  target: {
+    type: String as PropType<'_blank' | '_parent' | '_self' | '_top' | (string & object) | null | undefined>,
+    default: undefined,
+    required: false,
+  },
+})
+</script>
+
 <template>
   <NuxtLink
     :href="props.href"
@@ -7,19 +23,3 @@
     <slot />
   </NuxtLink>
 </template>
-
-<script setup lang="ts">
-import type { PropType } from 'vue'
-
-const props = defineProps({
-  href: {
-    type: String,
-    default: ''
-  },
-  target: {
-    type: String as PropType<'_blank' | '_parent' | '_self' | '_top' | (string & object) | null | undefined>,
-    default: undefined,
-    required: false
-  }
-})
-</script>
