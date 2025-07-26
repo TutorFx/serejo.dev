2a133bf623b6b300019d46be1daf4147e170ed8d
Gabriel Serejo <gabriel_serejo@live.com>
2025-07-26 10:41:54 -0300

    feat: added ProseA component

---
+++ app/components/content/ProseA.vue
@@ -0,0 +1,25 @@
+<template>
+  <NuxtLink
+    :href="props.href"
+    :target="props.target"
+    class="relative inline-block before:content-[''] before:absolute before:top-full before:w-full before:left-0 before:h-1 before:rounded-xs before:bg-linear-(--brand-gradient)"
+  >
+    <slot />
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+const props = defineProps({
+  href: {
+    type: String,
+    default: ''
+  },
+  target: {
+    type: String as PropType<'_blank' | '_parent' | '_self' | '_top' | (string & object) | null | undefined>,
+    default: undefined,
+    required: false
+  }
+})
+</script>
