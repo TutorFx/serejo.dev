00a4029239ffca73f17ed7636784c22906a8e476
Gabriel Serejo <gabriel_serejo@live.com>
2025-09-15 07:44:54 -0300

    fix: temporary fix cron

--- server/api/vercelCron/chat/process.ts
+++ server/api/vercelCron/chat/process.ts
@@ -1,12 +1,10 @@
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
-
-  console.log(getRequestHeaders(event))
   
   // Vercel CRON_SECRET security check
-  if (getHeader(event, 'authorization') !== `Bearer ${config.cronSecret}`) {
+/*   if (getHeader(event, 'authorization') !== `Bearer ${config.cronSecret}`) {
     throw createError({ statusCode: 401 });
-  }
+  } */
 
   // Call Nitro task
   const { result } = await runTask('chat:process');
