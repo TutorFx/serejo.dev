3ad446330ba15ec2953e535715773a0736a95c33
Gabriel Serejo <gabriel_serejo@live.com>
2026-05-22 09:53:42 -0300

    feat: add @stylistic/eslint-plugin and eslint-plugin-tailwindcss

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,35 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import pluginTailwindCSS from 'eslint-plugin-tailwindcss'
+import stylistic from '@stylistic/eslint-plugin'
 
-export default withNuxt({
-  rules: {
-    'vue/multi-word-component-names': 'off',
-    // 'vue/max-attributes-per-line': ['error', { singleline: 3 }],
-    'vue/max-attributes-per-line': 'off',
-    '@stylistic/comma-dangle': 'off',
-
-  }
-})
+export default withNuxt(
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/max-attributes-per-line': ['error', { singleline: 3 }],
+      '@stylistic/comma-dangle': 'off',
+      '@stylistic/indent': ['error', 2],
+    },
+    plugins: {
+      '@stylistic': stylistic,
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    plugins: {
+      tailwindcss: pluginTailwindCSS,
+    },
+    settings: {
+      tailwindcss: {
+        config: {},
+      },
+    },
+    rules: {
+      'tailwindcss/classnames-order': 'warn',
+      'tailwindcss/enforces-negative-arbitrary-values': 'warn',
+      'tailwindcss/enforces-shorthand': 'warn',
+      'tailwindcss/no-contradicting-classname': 'error',
+    },
+  },
+)
